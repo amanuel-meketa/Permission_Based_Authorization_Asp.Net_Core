a65b883bdeb4609c8d45adb2eef701f5bb0f4a02
Amanuel <amanuel.eshete1@gmail.com>
2023-06-06 12:49:47 +0300

    Add, Edit and Delete methods is now use dialogbox

--- Identity/Controllers/RoleController.cs
+++ Identity/Controllers/RoleController.cs
@@ -14,34 +14,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Identity.Controllers
 {
-
     public class RoleController : Controller
     {
         private readonly RoleManager<AppRole> _roleManager;
-
 
         public RoleController(RoleManager<AppRole> roleManager)
         {
             _roleManager = roleManager;
         }
 
-
         [HttpGet]
         [Authorize(Policy = Permissions.Permissions.Roles.View)]
-        public async Task<IActionResult> Index(int? pageNumber, int? pageSize)
+        public IActionResult Index()
         {
             var roles = _roleManager.Roles.OrderBy(x => x.Name);
-           
-            var rs = await PaginatedList<AppRole>.CreateFromEfQueryableAsync(roles.AsNoTracking(),
-                pageNumber ?? 1, pageSize ?? 12);
-            var rolesViewModel = rs.Select(role => new RoleViewModel
-                {Name = role.Name, Description = role.Description, Id = role.Id}).ToList();
+            var rolesViewModel = roles.Select(role => new RoleViewModel
+            {
+                Name = role.Name,
+                Description = role.Description,
+                Id = role.Id
+            }).ToList();
 
-            var response = new PaginatedList<RoleViewModel>(rolesViewModel, rs.Count, pageNumber ?? 1, pageSize ?? 12);
-      
-            return View(response);
+            return View(rolesViewModel);
         }
-
 
         [HttpGet]
         [Authorize(Policy = Permissions.Permissions.Roles.Create)]
@@ -50,12 +45,12 @@ namespace Identity.Controllers
             return View(new AddRoleViewModel());
         }
 
-
         [HttpPost]
         [Authorize(Policy = Permissions.Permissions.Roles.Create)]
         public async Task<IActionResult> Add(AddRoleViewModel addRoleViewModel)
         {
-            if (!ModelState.IsValid) return View(addRoleViewModel);
+            if (!ModelState.IsValid)
+                return View(addRoleViewModel);
 
             if (await _roleManager.FindByNameAsync(addRoleViewModel.Name) != null)
             {
@@ -63,42 +58,88 @@ namespace Identity.Controllers
                 return View(addRoleViewModel);
             }
 
-            var appRole = new AppRole(addRoleViewModel.Name) { Description = addRoleViewModel.Description};
+            var appRole = new AppRole(addRoleViewModel.Name) { Description = addRoleViewModel.Description };
             var rs = await _roleManager.CreateAsync(appRole);
             if (rs.Succeeded)
-                return RedirectToAction("Index", "Role",
-                    new {id = appRole.Id, succeeded = rs.Succeeded, message = rs.ToString()});
+                return RedirectToAction("Index", "Role", new { id = appRole.Id, succeeded = rs.Succeeded, message = rs.ToString() });
+
             ModelState.AddModelError(string.Empty, rs.ToString());
             return View(addRoleViewModel);
         }
 
-      
+        [HttpGet]
+        [Authorize(Policy = Permissions.Permissions.Roles.Edit)]
+        public async Task<IActionResult> Edit(string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+            if (role == null)
+                return NotFound();
+
+            var editRoleViewModel = new Role
+            {
+                RoleId = role.Id,
+                Name = role.Name,
+                Description = role.Description
+            };
+
+            return View(editRoleViewModel);
+        }
+
+        [HttpPost]
+        [Authorize(Policy = Permissions.Permissions.Roles.Edit)]
+        public async Task<IActionResult> Edit(Role editRoleViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(editRoleViewModel);
+
+            var role = await _roleManager.FindByIdAsync(editRoleViewModel.RoleId);
+            if (role == null)
+                return NotFound();
+
+            role.Name = editRoleViewModel.Name;
+            role.Description = editRoleViewModel.Description;
+
+            var result = await _roleManager.UpdateAsync(role);
+            if (result.Succeeded)
+                return RedirectToAction("Index");
+
+            ModelState.AddModelError(string.Empty, result.ToString());
+            return View(editRoleViewModel);
+        }
+
+
         [HttpGet]
         [Authorize(Policy = Permissions.Permissions.Roles.ManageClaims)]
         public async Task<IActionResult> ManageRolePermissions(string roleId, string permissionValue, int? pageNumber, int? pageSize)
         {
-
             var role = await _roleManager.FindByIdAsync(roleId);
-            if (role == null) return RedirectToAction("Index");
+            if (role == null)
+                return RedirectToAction("Index");
+
             var roleClaims = await _roleManager.GetClaimsAsync(role);
             var allPermissions = PermissionHelper.GetAllPermissions();
-            
+
             if (!string.IsNullOrWhiteSpace(permissionValue))
             {
                 allPermissions = allPermissions.Where(x => x.Value.ToLower().Contains(permissionValue.Trim().ToLower())).ToList();
             }
+
             var managePermissionsClaim = new List<ManageClaimViewModel>();
             foreach (var permission in allPermissions)
             {
-                var managePermissionClaim = new ManageClaimViewModel { Type = permission.Type, Value = permission.Value};
+                var managePermissionClaim = new ManageClaimViewModel { Type = permission.Type, Value = permission.Value };
                 if (roleClaims.Any(x => x.Value == permission.Value))
                 {
                     managePermissionClaim.Checked = true;
                 }
                 managePermissionsClaim.Add(managePermissionClaim);
             }
-            var paginatedList = PaginatedList<ManageClaimViewModel>.CreateFromLinqQueryable(managePermissionsClaim.AsQueryable(),
-                pageNumber ?? 1, pageSize ?? 12);
+
+            var paginatedList = PaginatedList<ManageClaimViewModel>.CreateFromLinqQueryable(
+                managePermissionsClaim.AsQueryable(),
+                pageNumber ?? 1,
+                pageSize ?? 12);
+
             var manageRolePermissionsViewModel = new ManageRolePermissionsViewModel
             {
                 RoleId = roleId,
@@ -107,45 +148,57 @@ namespace Identity.Controllers
                 ManagePermissionsViewModel = paginatedList
             };
 
-            if( allPermissions.Count > 0)
-                 return View(manageRolePermissionsViewModel);
+            if (allPermissions.Count > 0)
+                return View(manageRolePermissionsViewModel);
 
-            return RedirectToAction("Index", "Role", new
-                {succeeded = false, message = "No Permissions exists"});
-
+            return RedirectToAction("Index", "Role", new { succeeded = false, message = "No Permissions exist" });
         }
 
-
-       
         [HttpPost]
         [Authorize(Policy = Permissions.Permissions.Roles.ManageClaims)]
         public async Task<IActionResult> ManageRoleClaims(ManageRoleClaimViewModel manageRoleClaimViewModel)
         {
             if (!ModelState.IsValid)
-                return Json(new {Succeeded  =false, Messaege="failed"});
+                return Json(new { Succeeded = false, Message = "Failed" });
+
             var roleById = await _roleManager.FindByIdAsync(manageRoleClaimViewModel.RoleId);
             var roleByName = await _roleManager.FindByNameAsync(manageRoleClaimViewModel.RoleName);
-            if(roleById != roleByName)
-                return Json(new { Succeeded = false, Messaege = "failed" });
+
+            if (roleById != roleByName)
+                return Json(new { Succeeded = false, Message = "Failed" });
+
             var allClaims = await _roleManager.GetClaimsAsync(roleById);
-            var claimExists =
-                allClaims.Where(x => x.Type == manageRoleClaimViewModel.Type && x.Value == manageRoleClaimViewModel.Value).ToList();
+            var claimExists = allClaims.Where(x => x.Type == manageRoleClaimViewModel.Type && x.Value == manageRoleClaimViewModel.Value).ToList();
+
             switch (manageRoleClaimViewModel.Checked)
             {
                 case true when claimExists.Count == 0:
                     await _roleManager.AddClaimAsync(roleById, new Claim(manageRoleClaimViewModel.Type, manageRoleClaimViewModel.Value));
                     break;
                 case false when claimExists.Count > 0:
-                {
                     foreach (var claim in claimExists)
                     {
                         await _roleManager.RemoveClaimAsync(roleById, claim);
                     }
                     break;
-                }
             }
-            return Json(new {Succeeded = true, Message="Success"});
+
+            return Json(new { Succeeded = true, Message = "Success" });
         }
 
+        [HttpPost]
+        [Authorize(Policy = Permissions.Permissions.Roles.Delete)]
+        public async Task<IActionResult> DeleteRoleConfirmed(string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+            if (role == null)
+                return NotFound();
+
+            var result = await _roleManager.DeleteAsync(role);
+            if (result.Succeeded)
+                return RedirectToAction("Index");
+
+            return RedirectToAction("Index", new { succeeded = false, message = result.ToString() });
+        }
     }
 }
---
+++ Identity/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models
+{
+    public class Role
+    {
+        public string RoleId { get; set; }
+
+        [Required(ErrorMessage = "The role name is required.")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
